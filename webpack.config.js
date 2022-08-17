a62152a90a060d386be24749bc2de5754c85596d
nicey <1919325005@qq.com>
2022-08-17 10:31:11 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
@@ -12,16 +13,18 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        // use: ["style-loader", "css-loader"],
       },
     ],
   },
   entry: "./src/index.js",
   plugins: [
-    new HtmlWebpackPlugin({
-      title: "nicey",
-      template: "src/assets/index.html",
-    }),
+    // new HtmlWebpackPlugin({
+    //   title: "nicey",
+    //   template: "src/assets/index.html",
+    // }),
+    new MiniCssExtractPlugin(),
   ],
   output: {
     path: path.resolve(__dirname, "dist"),
