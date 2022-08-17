c18d10cd41f42f9ead48cb162baabec214e1cae7
nicey <1919325005@qq.com>
2022-08-17 10:09:00 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const path = require("path");
 module.exports = {
   mode: "development", // 开发者模式
   // mode: "production", // 用户模式，正要发布之前改为这个
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   module: {
     rules: [
       {
@@ -13,14 +17,17 @@ module.exports = {
     ],
   },
   entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index .[contenthash].js",
-  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "nicey",
       template: "src/assets/index.html",
     }),
   ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index .[contenthash].js",
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
 };
