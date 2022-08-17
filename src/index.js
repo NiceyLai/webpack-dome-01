6eddfbc7b79b3ff0ae77aa2ac595436c96fef89c
nicey <1919325005@qq.com>
2022-08-17 09:42:18 +0800

    updata

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import x from "./x.js";
 console.log(x);
 console.log("123");
+console.log("第二次实验");
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
-var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   mode: "development", // 开发者模式
   // mode: "production", // 用户模式，正要发布之前改为这个
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    filename: "index .[contenthash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "nicey",
+      template: "src/assets/index.html",
+    }),
+  ],
 };
